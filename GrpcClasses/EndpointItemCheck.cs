c16b1ef412e7b9a547e943e7ed69ed7c5ebe6f38
Arthur Frey <afrey@steelcloud.com>
2020-06-19 12:06:33 -0400

    Fixed

---
+++ GrpcClasses/EndpointItemCheck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace GrpcClasses
+{
+    public class EndpointItemCheck
+    {
+        public EndpointItem Endpoint { get; set; } = new EndpointItem();
+        public DateTime CheckDateTime { get; set; } = DateTime.Now;
+        public bool Result { get; set; } = false;
+        public string Message { get; set; } = String.Empty;
+    }
+
+}
---
+++ GrpcClasses/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace GrpcClasses
+{
+
+    public static class Utils
+    {
+        public static List<EndpointItem> CreateEndpointList(int count)
+        {
+            List<EndpointItem> EndpointItemList = new List<EndpointItem>();
+            List<string> PlatformList = new List<string>() { "windows", "linux", "other" };
+
+            for (int i = 0; i < count; i++)
+            {
+                Random rand = new Random();
+                EndpointItemList.Add(new EndpointItem()
+                {
+                    Name = "Test " + i.ToString(),
+                    IpAddress = string.Format("{0}.{1}.{2}.{3}", rand.Next(1, 256), rand.Next(1, 256), rand.Next(1, 256), rand.Next(1, 256)),
+                    Platform = PlatformList[rand.Next(PlatformList.Count)]
+                });
+            }
+
+            return EndpointItemList;
+        }
+    }
+
+}
